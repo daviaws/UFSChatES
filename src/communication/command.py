0f54c847e1c24f924f93eb192d59f61113df99a2
daviaws <davi.abreu.w@gmail.com>
2016-04-17 21:09:11 -0300

    Reestruturando diretorios e atualizando comandos

--- src/communication/command.py
+++ src/communication/command.py
@@ -4,22 +4,37 @@ from datetime import datetime
 
 class Command():
 
-	def __init__(self, cmd, **args):
+	def __init__(self, cmd, args=None):
+		print(cmd)
 
 		self.cmd = cmd
 		self.date = str(datetime.now())
 		self.args = args
+		self.keyargs = []
 
-	#abstract
 	def validate(self):
-		pass
+		if self.keyargs:
+			if not list(self.args) == self.keyargs:
+				return False
+		return True
 
-	#abstract
 	def encode(self):
-		pass
+		if self.args:
+			encodedCommand = {'cmd' : self.cmd, 'datetime' : self.date}
+		else:
+			encodedCommand = {'cmd' : self.cmd, 'datetime' : self.date, 'params' : self.args}
+		encodedCommand = json.dumps(encodedCommand)
 
 	#abstract
 	def decode(self):
 		pass
 
-a = Command('oi', user='name', passwd='1234')
+class Register(Command):
+
+	def __init__(self, cmd, **args):
+		print(cmd)
+		super().__init__(cmd, args)
+		self.keyargs = ['username', 'login']
+
+a = Register('oi', user='name', passwd='1234')
+print(a)
