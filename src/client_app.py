9da39fd7419a4e07d2272d13947caf6b9cf8eb11
daviaws <davi.abreu.w@gmail.com>
2016-05-15 21:18:32 -0300

    Stable gui

--- src/client_app.py
+++ src/client_app.py
@@ -135,7 +135,7 @@ class ChatWindow():
         self.center()
         self.root.withdraw()
         self.root.title('Add contact')
-        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.root.protocol("WM_DELETE_WINDOW", self.on_close)
 
         addContactFrame = Frame(self.root, bd=1, relief=SUNKEN)
         addContactFrame.pack(anchor=CENTER, fill=BOTH)
@@ -144,12 +144,12 @@ class ChatWindow():
         addContactLabel.pack()
         self.addContactEntry = Entry(addContactFrame)
         self.addContactEntry.pack()
-        ################################TODO
         addContactButton = Button(addContactFrame, text='Add', command= lambda: self.master.update(event_pressed_add_contact, username=self.get_entry()))
         addContactButton.pack()
 
     def get_entry(self):
         entry = self.addContactEntry.get()
+        self.clear()
         print('Entry is %s' % entry)
         return entry
 
@@ -162,36 +162,42 @@ class ChatWindow():
         y = h/2 - size[1]/2
         self.root.geometry("%dx%d+%d+%d" % (size + (x, y)))
 
-    def on_closing(self):
+    def clear(self):
+        print('Era pra limpar :c')
+        self.addContactEntry.delete(0, END)
+
+    def on_close(self):
+        self.clear()
         self.root.withdraw()
 
     def raises(self):
+        self.on_close
         self.center()
         self.root.deiconify()
 
 class AddContactWindow():
     
     WIDTHPIXELS = 200
-    HEIGHTPIXELS = 80
+    HEIGHTPIXELS = 70   
 
     def __init__(self, master):
         self.master = master
         self.root = Tk()
         self.root.geometry('%dx%d' % (self.WIDTHPIXELS, self.HEIGHTPIXELS))
-        self.root.resizable(0, 0)
         self.center()
+        self.root.resizable(0, 0)
         self.root.withdraw()
         self.root.title('Add contact')
-        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.root.protocol("WM_DELETE_WINDOW", self.on_close)
 
         addContactFrame = Frame(self.root, bd=1, relief=SUNKEN)
         addContactFrame.pack(anchor=CENTER, fill=BOTH)
         
         addContactLabel = Label(addContactFrame, text="Add Contact")
         addContactLabel.pack()
-        addContactEntry = Entry(addContactFrame)
-        addContactEntry.pack()
-        addContactButton = Button(addContactFrame, text='Add', command= lambda: self.master.update(event_pressed_add_contact, username=addContactEntry.get()))
+        self.addContactEntry = Entry(addContactFrame)
+        self.addContactEntry.pack()
+        addContactButton = Button(addContactFrame, text='Add', command= lambda: self.master.update(event_pressed_add_contact, username=self.get_entry()))
         addContactButton.pack()
 
     def center(self):
@@ -203,10 +209,21 @@ class AddContactWindow():
         y = h/2 - size[1]/2
         self.root.geometry("%dx%d+%d+%d" % (size + (x, y)))
 
-    def on_closing(self):
+    def get_entry(self):
+        entry = self.addContactEntry.get()
+        self.clear()
+        print('Entry is %s' % entry)
+        return entry
+
+    def clear(self):
+        self.addContactEntry.delete(0, END)
+
+    def on_close(self):
+        self.clear()
         self.root.withdraw()
 
     def raises(self):
+        self.on_close()
         self.center()
         self.root.deiconify()
 
